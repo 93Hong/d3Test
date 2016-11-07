df55639890767a400f14980a5f8df197db326385
93Hong <sk2rldnr@gmail.com>
2016-11-07 23:29:10 +0900

    backup_before title

--- app.js
+++ app.js
@@ -1,38 +1,376 @@
 var express = require('express');
-var path = require('path');
 var app = express();
 var fs = require('fs');
 var json2csv = require('json2csv');
-var args = process.argv.slice(2);
+var path = require('path');
+//var args = process.argv.slice(2);
+var chart = require('./chart');
+//app.use(express.static(__dirname + "/public"));
+var bodyParser = require('body-parser');
+var schedule = require('node-schedule');
 
-fs.readFile('C:\\Data\\crawl\\komoran\\komoResult1.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    var obj = JSON.parse(data);
+app.use(bodyParser.json()); // 2
+app.use(bodyParser.urlencoded({extended:true})); // 3
 
-    var fields = ['age', 'population'];
-    var tsvData = [];
+var dirname = "/usr/data/crawl/result/"; //"/usr/data/crawl/result/";//
+var naver = "naver/";
+var daum = "daum/";
+var twitter = "twitter/";
+var naverCumulative = "naverCumulative/";
+var daumCumulative = "daumCumulative/";
+var twitterCumulative = "twitterCumulative/";
 
-    for (var i = 0; i < args.length; i++) {      
-        var json = new Object();
-        json.age = args[i];
-        json.population = obj[args[i]];
-        tsvData.push(json);
-    }
-    console.log(tsvData);
+var j = schedule.scheduleJob('0 */30 * * * *', function() {//every 30 minutes
+  var str = fs.readFileSync('/usr/data/crawl/komoran/twitter/word.txt') + '';
+  var args = str.split("/");
+  var fields = ['date'];
 
-    var tsv = json2csv({ data: tsvData, fields: fields, del: '\t' });
-    fs.writeFile('./public/data.tsv', tsv, function(err) {
-        if (err) throw err;
-        console.log('file saved');
+  args.forEach(function(data) {
+      fields.push(data);
+  });
+
+  // naver
+  fs.readdir(dirname + naver, function(err, files) {
+      if (err) {
+          console.error("Could not list the directory.", err);
+          throw err;
+      }
+      fs.readFile("/usr/data/crawl/komoran/naver/index.txt", 'utf-8', function(err, content) {
+          if (err) {
+              console.error("Could not list the directory.", err);
+              throw err;
+          }
+
+          var start = content;
+          var tsvData = [];
+          var size = files.length;
+          start++;
+          if (start == size)
+              start = 0;
+
+          for (var i = 0; i < size; i++) {
+              var data = fs.readFileSync(dirname + naver + start + ".txt", 'utf-8');
+              var obj = JSON.parse(data);
+              var json = {};
+
+              json.date = 0.5 * (size - i - 1);
+              for (var j = 0; j < args.length; j++) {
+                  var tmp = obj[args[j]];
+                  if (typeof tmp == 'undefined')
+                      tmp = 0;
+                  json[args[j]] = tmp;
+              }
+              tsvData.push(json);
+
+              if (start == size - 1)
+                  start = 0;
+              else
+                  start++;
+          }
+          var tsv = json2csv({
+              data: tsvData,
+              fields: fields,
+              del: '\t'
+          });
+          fs.writeFile('./public/naver.tsv', tsv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+          });
+      });
+  });
+
+  // naverCumulative
+  fs.readdir(dirname + naverCumulative, function(err, files) {
+      if (err) {
+          console.error("Could not list the directory.", err);
+          throw err;
+      }
+      fs.readFile("/usr/data/crawl/result/naverCumulative/index.txt", 'utf-8', function(err, content) {
+          if (err) {
+              console.error("Could not list the directory.", err);
+              throw err;
+          }
+
+          var start = content;
+          var tsvData = [];
+          var size = files.length - 1;
+          start++;
+          if (start == size)
+              start = 0;
+
+          for (var i = 0; i < size; i++) {
+              var str = "" + start;
+              var pad = "000";
+              var ans = pad.substring(0, pad.length - str.length) + str;
+              var data = fs.readFileSync(dirname + naverCumulative + ans + ".txt", 'utf-8');
+              var obj = JSON.parse(data);
+              var json = {};
+
+              if (size - 1 == i)
+                  json.date = 0;
+              else
+                  json.date = (size - i) / 48;
+              for (var j = 0; j < args.length; j++) {
+                  var tmp = obj[args[j]];
+                  if (typeof tmp == 'undefined')
+                      tmp = 0;
+                  json[args[j]] = tmp;
+              }
+              tsvData.push(json);
+
+              if (start == size - 1)
+                  start = 0;
+              else
+                  start++;
+          }
+          var tsv = json2csv({
+              data: tsvData,
+              fields: fields,
+              del: '\t'
+          });
+          fs.writeFile('./public/naverCumulative.tsv', tsv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+          });
+      });
+  });
+
+  // daum
+  fs.readdir(dirname + daum, function(err, files) {
+      if (err) {
+          console.error("Could not list the directory.", err);
+          throw err;
+      }
+      fs.exists("/usr/data/crawl/komoran/daum/index.txt", function(fileok) {
+          if(fileok)fs.readFile("/usr/data/crawl/komoran/daum/index.txt", 'utf-8', function(err, content) {
+          if (err) {
+              console.error("Could not list the directory.", err);
+              throw err;
+          }
+
+          var start = content;
+          var tsvData = [];
+          var size = files.length;
+          start++;
+          if (start == size)
+              start = 0;
+
+          for (var i = 0; i < size; i++) {
+              var data = fs.readFileSync(dirname + daum + start + ".txt", 'utf-8');
+              var obj = JSON.parse(data);
+              var json = {};
+
+              json.date = 0.5 * (size - i - 1);
+              for (var j = 0; j < args.length; j++) {
+                  var tmp = obj[args[j]];
+                  if (typeof tmp == 'undefined')
+                      tmp = 0;
+                  json[args[j]] = tmp;
+              }
+              tsvData.push(json);
+
+              if (start == size - 1)
+                  start = 0;
+              else
+                  start++;
+          }
+          var tsv = json2csv({
+              data: tsvData,
+              fields: fields,
+              del: '\t'
+          });
+          fs.writeFile('./public/daum.tsv', tsv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+          });
+      });
     });
+  });
+
+  // daumCumulative
+  fs.readdir(dirname + daumCumulative, function(err, files) {
+      if (err) {
+          console.error("Could not list the directory.", err);
+          throw err;
+      }
+      fs.readFile("/usr/data/crawl/result/daumCumulative/index.txt", 'utf-8', function(err, content) {
+          if (err) {
+              console.error("Could not list the directory.", err);
+              throw err;
+          }
+
+          var start = content;
+          var tsvData = [];
+          var size = files.length - 1;
+          start++;
+          if (start == size)
+              start = 0;
+
+          for (var i = 0; i < size; i++) {
+              var str = "" + start;
+              var pad = "000";
+              var ans = pad.substring(0, pad.length - str.length) + str;
+              var data = fs.readFileSync(dirname + daumCumulative + ans + ".txt", 'utf-8');
+              var obj = JSON.parse(data);
+              var json = {};
+
+              if (size - 1 == i)
+                  json.date = 0;
+              else
+                  json.date = (size - i) / 48;
+              for (var j = 0; j < args.length; j++) {
+                  var tmp = obj[args[j]];
+                  if (typeof tmp == 'undefined')
+                      tmp = 0;
+                  json[args[j]] = tmp;
+              }
+              tsvData.push(json);
+
+              if (start == size - 1)
+                  start = 0;
+              else
+                  start++;
+          }
+          var tsv = json2csv({
+              data: tsvData,
+              fields: fields,
+              del: '\t'
+          });
+          fs.writeFile('./public/daumCumulative.tsv', tsv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+          });
+      });
+  });
+
+  // twitter
+  fs.readdir(dirname + twitter, function(err, files) {
+      if (err) {
+          console.error("Could not list the directory.", err);
+          throw err;
+      }
+      fs.readFile("/usr/data/crawl/komoran/twitter/index.txt", 'utf-8', function(err, content) {
+          if (err) {
+              console.error("Could not list the directory.", err);
+              throw err;
+          }
+
+          var start = content;
+          var tsvData = [];
+          var size = files.length;
+          start++;
+          if (start == size)
+              start = 0;
+
+          for (var i = 0; i < size; i++) {
+              var data = fs.readFileSync(dirname + twitter + start + ".txt", 'utf-8');
+              var obj = JSON.parse(data);
+              var json = {};
+
+              json.date = 0.5 * (size - i - 1);
+              for (var j = 0; j < args.length; j++) {
+                  var tmp = obj[args[j]];
+                  if (typeof tmp == 'undefined')
+                      tmp = 0;
+                  json[args[j]] = tmp;
+              }
+              tsvData.push(json);
+
+              if (start == size - 1)
+                  start = 0;
+              else
+                  start++;
+          }
+          var tsv = json2csv({
+              data: tsvData,
+              fields: fields,
+              del: '\t'
+          });
+          fs.writeFile('./public/twitter.tsv', tsv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+          });
+      });
+  });
+
+  // twitterCumulative
+  fs.readdir(dirname + twitterCumulative, function(err, files) {
+      if (err) {
+          console.error("Could not list the directory.", err);
+          throw err;
+      }
+      fs.readFile("/usr/data/crawl/result/twitterCumulative/index.txt", 'utf-8', function(err, content) {
+          if (err) {
+              console.error("Could not list the directory.", err);
+              throw err;
+          }
+
+          var start = content;
+          var tsvData = [];
+          var size = files.length - 1;
+          start++;
+          if (start == size)
+              start = 0;
+
+          for (var i = 0; i < size; i++) {
+              var str = "" + start;
+              var pad = "000";
+              var ans = pad.substring(0, pad.length - str.length) + str;
+              var data = fs.readFileSync(dirname + twitterCumulative + ans + ".txt", 'utf-8');
+              var obj = JSON.parse(data);
+              var json = {};
+
+              if (size - 1 == i)
+                  json.date = 0;
+              else
+                  json.date = (size - i) / 48;
+              for (var j = 0; j < args.length; j++) {
+                  var tmp = obj[args[j]];
+                  if (typeof tmp == 'undefined')
+                      tmp = 0;
+                  json[args[j]] = tmp;
+              }
+              tsvData.push(json);
+
+              if (start == size - 1)
+                  start = 0;
+              else
+                  start++;
+          }
+          var tsv = json2csv({
+              data: tsvData,
+              fields: fields,
+              del: '\t'
+          });
+          fs.writeFile('./public/twitterCumulative.tsv', tsv, function(err) {
+              if (err) throw err;
+              console.log('file saved');
+          });
+      });
+  });
+
 });
 
-app.use(express.static(__dirname + '/public'));
+app.get("/create", function(req, res) {
+    res.sendFile(__dirname + "/form.html");
+});
 
-//app.get('/', function(req, res) {
-//  res.send('he');
-//});
 
-app.listen(8000, function() {
-  console.log('server');
+app.post("/create", function(req, res) {
+    var arr = req.body.name.filter(function(n){ return n !== ""; });
+    var data = arr.join('/');
+
+    fs.writeFileSync('/usr/data/crawl/komoran/twitter/word.txt', data);
+
+    res.redirect('/');
+});
+
+app.get("/", function(req, res) {
+    res.redirect('/host');
+});
+
+app.use('/host', chart);
+
+app.listen(8080, function() {
+    console.log('server');
 });
